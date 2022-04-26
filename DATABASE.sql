f2d688747b022c5d58f1f3643f050659176af7b7
minknhom <19521844@gm.uit.edu.vn>
2022-04-27 00:07:35 +0700

    sach tieu thuyet

--- DATABASE.sql
+++ DATABASE.sql
@@ -235,3 +235,110 @@ insert into thedocgia values (17,'Phạm Hồng Phước','Giảng Viên',str_to
 insert into thedocgia values (18,'Đoàn Thùy Trang','Giảng Viên',str_to_date('23/1/1994','%d/%m/%Y'),'KTX Khu A, ĐHQG','dttrang@gmail.com',str_to_date('26/4/2022','%d/%m/%Y'),'Còn hạn');
 insert into thedocgia values (19,'Nguyễn Thị Nhân','Giảng Viên',str_to_date('25/6/1993','%d/%m/%Y'),'KTX Khu A, ĐHQG','ntnhan@gmail.com',str_to_date('26/4/2022','%d/%m/%Y'),'Còn hạn');
 insert into thedocgia values (20,'Nguyễn Thị Hồng','Giảng Viên',str_to_date('23/1/1995','%d/%m/%Y'),'KTX Khu A, ĐHQG','nthong@gmail.com',str_to_date('26/4/2022','%d/%m/%Y'),'Còn hạn');
+
+
+
+
+INSERT INTO theloai (`ID_THELOAI`, `TENTHELOAI`) VALUES ('3', 'Sách tiểu thuyết');
+
+
+
+INSERT INTO dausach (`ID_DAUSACH`, `ID_THELOAI`, `TENSACH`, `NAMXUATBAN`, `NHAXUATBAN`, `NGNHAP`, `TRIGIA`, `TONGSO`, `SANCO`, `DANGCHOMUON`, `HINHANH`) VALUES ('31', '3', 'Luật tâm thức', '2021', 'Nhà Xuất Bản Dân Trí', '2022-04-26', '180439', '3', '3', '0', '/HinhAnh/31.jpg');
+INSERT INTO dausach (`ID_DAUSACH`, `ID_THELOAI`, `TENSACH`, `NAMXUATBAN`, `NHAXUATBAN`, `NGNHAP`, `TRIGIA`, `TONGSO`, `SANCO`, `DANGCHOMUON`, `HINHANH`) VALUES ('32', '3', 'Hội chứng E', '2020', 'NXB Hội Nhà Văn', '2022-04-26', '288500', '3', '3', '0', '/HinhAnh/32.jpg');
+INSERT INTO dausach (`ID_DAUSACH`, `ID_THELOAI`, `TENSACH`, `NAMXUATBAN`, `NHAXUATBAN`, `NGNHAP`, `TRIGIA`, `TONGSO`, `SANCO`, `DANGCHOMUON`, `HINHANH`) VALUES ('33', '3', 'Án Mạng Mười Một Chữ', '2020', 'Nhà Xuất Bản Hội Nhà Văn', '2022-04-26', '288500', '3', '3', '0', '/HinhAnh/33.jpg');
+INSERT INTO dausach (`ID_DAUSACH`, `ID_THELOAI`, `TENSACH`, `NAMXUATBAN`, `NHAXUATBAN`, `NGNHAP`, `TRIGIA`, `TONGSO`, `SANCO`, `DANGCHOMUON`, `HINHANH`) VALUES ('34', '3', 'Nỗi buồn chiến tranh', '2017', 'NXB Trẻ', '2022-04-26', '115000', '3', '3', '0', '/HinhAnh/34.jpg');
+INSERT INTO dausach (`ID_DAUSACH`, `ID_THELOAI`, `TENSACH`, `NAMXUATBAN`, `NHAXUATBAN`, `NGNHAP`, `TRIGIA`, `TONGSO`, `SANCO`, `DANGCHOMUON`, `HINHANH`) VALUES ('35', '3', 'Những ngày thơ ấu', '2021', 'Nhà Xuất Bản Hội Nhà Văn', '2022-04-26', '54800', '3', '3', '0', '/HinhAnh/35.jpg');
+INSERT INTO dausach (`ID_DAUSACH`, `ID_THELOAI`, `TENSACH`, `NAMXUATBAN`, `NHAXUATBAN`, `NGNHAP`, `TRIGIA`, `TONGSO`, `SANCO`, `DANGCHOMUON`, `HINHANH`) VALUES ('36', '3', 'Không gia đình', '2022', 'Nhà Xuất Bản Mỹ Thuật', '2022-04-26', '84400', '3', '3', '0', '/HinhAnh/36.jpg');
+INSERT INTO dausach (`ID_DAUSACH`, `ID_THELOAI`, `TENSACH`, `NAMXUATBAN`, `NHAXUATBAN`, `NGNHAP`, `TRIGIA`, `TONGSO`, `SANCO`, `DANGCHOMUON`, `HINHANH`) VALUES ('37', '3', 'Suối nguồn', '2018', 'NXB Trẻ', '2022-04-26', '230800', '3', '3', '0', '/HinhAnh/37.jpg');
+INSERT INTO dausach (`ID_DAUSACH`, `ID_THELOAI`, `TENSACH`, `NAMXUATBAN`, `NHAXUATBAN`, `NGNHAP`, `TRIGIA`, `TONGSO`, `SANCO`, `DANGCHOMUON`, `HINHANH`) VALUES ('38', '3', 'Bố già', '2016', 'Nhà Xuất Bản Văn Học', '2022-04-26', '63923', '3', '3', '0', '/HinhAnh/38.jpg');
+INSERT INTO dausach (`ID_DAUSACH`, `ID_THELOAI`, `TENSACH`, `NAMXUATBAN`, `NHAXUATBAN`, `NGNHAP`, `TRIGIA`, `TONGSO`, `SANCO`, `DANGCHOMUON`, `HINHANH`) VALUES ('39', '3', 'Hai số phận', '2018', 'Nhà Xuất Bản Văn Học', '2022-04-26', '111000', '3', '3', '0', '/HinhAnh/39.jpg');
+INSERT INTO dausach (`ID_DAUSACH`, `ID_THELOAI`, `TENSACH`, `NAMXUATBAN`, `NHAXUATBAN`, `NGNHAP`, `TRIGIA`, `TONGSO`, `SANCO`, `DANGCHOMUON`, `HINHANH`) VALUES ('40', '3', 'Nhà giả kim', '2020', 'Nhà Xuất Bản Hà Nội', '2022-04-26', '53544', '3', '3', '0', '/HinhAnh/40.jpg');
+INSERT INTO dausach (`ID_DAUSACH`, `ID_THELOAI`, `TENSACH`, `NAMXUATBAN`, `NHAXUATBAN`, `NGNHAP`, `TRIGIA`, `TONGSO`, `SANCO`, `DANGCHOMUON`, `HINHANH`) VALUES ('41', '3', 'Điều kì diệu ', '2019', 'NXB Trẻ', '2022-04-26', '118400', '3', '3', '0', '/HinhAnh/41.jpg');
+INSERT INTO dausach (`ID_DAUSACH`, `ID_THELOAI`, `TENSACH`, `NAMXUATBAN`, `NHAXUATBAN`, `NGNHAP`, `TRIGIA`, `TONGSO`, `SANCO`, `DANGCHOMUON`, `HINHANH`) VALUES ('42', '3', 'Những đêm không ngủ những ngày chậm trôi', '2021', 'Nhà Xuất Bản Văn Học', '2022-04-26', '54960 ', '3', '3', '0', '/HinhAnh/42.jpg');
+INSERT INTO dausach (`ID_DAUSACH`, `ID_THELOAI`, `TENSACH`, `NAMXUATBAN`, `NHAXUATBAN`, `NGNHAP`, `TRIGIA`, `TONGSO`, `SANCO`, `DANGCHOMUON`, `HINHANH`) VALUES ('43', '3', 'Rừng Nauy', '2021', 'Nhà Xuất Bản Hội Nhà Văn', '2022-04-26', '109500', '3', '3', '0', '/HinhAnh/43.jpg');
+INSERT INTO dausach (`ID_DAUSACH`, `ID_THELOAI`, `TENSACH`, `NAMXUATBAN`, `NHAXUATBAN`, `NGNHAP`, `TRIGIA`, `TONGSO`, `SANCO`, `DANGCHOMUON`, `HINHANH`) VALUES ('44', '3', 'Đi tìm lẽ sống', '2019', 'NXB Tổng Hợp TPHCM', '2022-04-26', '62400', '3', '3', '0', '/HinhAnh/44.jpg');
+INSERT INTO dausach (`ID_DAUSACH`, `ID_THELOAI`, `TENSACH`, `NAMXUATBAN`, `NHAXUATBAN`, `NGNHAP`, `TRIGIA`, `TONGSO`, `SANCO`, `DANGCHOMUON`, `HINHANH`) VALUES ('45', '3', 'Trà hoa nữ', '2015', 'NXB Văn Học', '2022-04-26', '55800', '3', '3', '0', '/HinhAnh/45.jpg');
+
+
+
+INSERT INTO tacgia (`ID_TACGIA`, `TENTG`) VALUES ('36', 'Ngô Sa Thạch');
+INSERT INTO tacgia (`ID_TACGIA`, `TENTG`) VALUES ('37', 'Franck Thilliez');
+INSERT INTO tacgia (`ID_TACGIA`, `TENTG`) VALUES ('38', 'Higashino Keigo');
+INSERT INTO tacgia (`ID_TACGIA`, `TENTG`) VALUES ('39', 'Bảo Ninh');
+INSERT INTO tacgia (`ID_TACGIA`, `TENTG`) VALUES ('40', 'Nguyên Hồng');
+INSERT INTO tacgia (`ID_TACGIA`, `TENTG`) VALUES ('41', 'Hector Malot');
+INSERT INTO tacgia (`ID_TACGIA`, `TENTG`) VALUES ('42', 'Ayn Rand');
+INSERT INTO tacgia (`ID_TACGIA`, `TENTG`) VALUES ('43', 'Mario Puzo');
+INSERT INTO tacgia (`ID_TACGIA`, `TENTG`) VALUES ('44', 'Jeffrey Archer');
+INSERT INTO tacgia (`ID_TACGIA`, `TENTG`) VALUES ('45', 'Paulo Coelho');
+INSERT INTO tacgia (`ID_TACGIA`, `TENTG`) VALUES ('46', 'R.J.Palacio');
+INSERT INTO tacgia (`ID_TACGIA`, `TENTG`) VALUES ('47', 'A Crazy Mind team');
+INSERT INTO tacgia (`ID_TACGIA`, `TENTG`) VALUES ('48', 'Haruki Murakami');
+INSERT INTO tacgia (`ID_TACGIA`, `TENTG`) VALUES ('49', 'Viktor Emil Frankl');
+INSERT INTO tacgia (`ID_TACGIA`, `TENTG`) VALUES ('50', 'Alexandre Dumas');
+
+
+
+INSERT INTO sach (`ID_SACH`, `ID_DAUSACH`, `ATTRIBUTE_22`) VALUES ('91', '31', 'sẵn có');
+INSERT INTO sach (`ID_SACH`, `ID_DAUSACH`, `ATTRIBUTE_22`) VALUES ('92', '31', 'sẵn có');
+INSERT INTO sach (`ID_SACH`, `ID_DAUSACH`, `ATTRIBUTE_22`) VALUES ('93', '31', 'sẵn có');
+INSERT INTO sach (`ID_SACH`, `ID_DAUSACH`, `ATTRIBUTE_22`) VALUES ('94', '32', 'sẵn có');
+INSERT INTO sach (`ID_SACH`, `ID_DAUSACH`, `ATTRIBUTE_22`) VALUES ('95', '32', 'sẵn có');
+INSERT INTO sach (`ID_SACH`, `ID_DAUSACH`, `ATTRIBUTE_22`) VALUES ('96', '32', 'sẵn có');
+INSERT INTO sach (`ID_SACH`, `ID_DAUSACH`, `ATTRIBUTE_22`) VALUES ('97', '33', 'sẵn có');
+INSERT INTO sach (`ID_SACH`, `ID_DAUSACH`, `ATTRIBUTE_22`) VALUES ('98', '33', 'sẵn có');
+INSERT INTO sach (`ID_SACH`, `ID_DAUSACH`, `ATTRIBUTE_22`) VALUES ('99', '33', 'sẵn có');
+INSERT INTO sach (`ID_SACH`, `ID_DAUSACH`, `ATTRIBUTE_22`) VALUES ('100', '34', 'sẵn có');
+INSERT INTO sach (`ID_SACH`, `ID_DAUSACH`, `ATTRIBUTE_22`) VALUES ('101', '34', 'sẵn có');
+INSERT INTO sach (`ID_SACH`, `ID_DAUSACH`, `ATTRIBUTE_22`) VALUES ('102', '34', 'sẵn có');
+INSERT INTO sach (`ID_SACH`, `ID_DAUSACH`, `ATTRIBUTE_22`) VALUES ('103', '35', 'sẵn có');
+INSERT INTO sach (`ID_SACH`, `ID_DAUSACH`, `ATTRIBUTE_22`) VALUES ('104', '35', 'sẵn có');
+INSERT INTO sach (`ID_SACH`, `ID_DAUSACH`, `ATTRIBUTE_22`) VALUES ('105', '35', 'sẵn có');
+INSERT INTO sach (`ID_SACH`, `ID_DAUSACH`, `ATTRIBUTE_22`) VALUES ('106', '36', 'sẵn có');
+INSERT INTO sach (`ID_SACH`, `ID_DAUSACH`, `ATTRIBUTE_22`) VALUES ('107', '36', 'sẵn có');
+INSERT INTO sach (`ID_SACH`, `ID_DAUSACH`, `ATTRIBUTE_22`) VALUES ('108', '36', 'sẵn có');
+INSERT INTO sach (`ID_SACH`, `ID_DAUSACH`, `ATTRIBUTE_22`) VALUES ('109', '37', 'sẵn có');
+INSERT INTO sach (`ID_SACH`, `ID_DAUSACH`, `ATTRIBUTE_22`) VALUES ('110', '37', 'sẵn có');
+INSERT INTO sach (`ID_SACH`, `ID_DAUSACH`, `ATTRIBUTE_22`) VALUES ('111', '37', 'sẵn có');
+INSERT INTO sach (`ID_SACH`, `ID_DAUSACH`, `ATTRIBUTE_22`) VALUES ('112', '38', 'sẵn có');
+INSERT INTO sach (`ID_SACH`, `ID_DAUSACH`, `ATTRIBUTE_22`) VALUES ('113', '38', 'sẵn có');
+INSERT INTO sach (`ID_SACH`, `ID_DAUSACH`, `ATTRIBUTE_22`) VALUES ('114', '38', 'sẵn có');
+INSERT INTO sach (`ID_SACH`, `ID_DAUSACH`, `ATTRIBUTE_22`) VALUES ('115', '39', 'sẵn có');
+INSERT INTO sach (`ID_SACH`, `ID_DAUSACH`, `ATTRIBUTE_22`) VALUES ('116', '39', 'sẵn có');
+INSERT INTO sach (`ID_SACH`, `ID_DAUSACH`, `ATTRIBUTE_22`) VALUES ('117', '39', 'sẵn có');
+INSERT INTO sach (`ID_SACH`, `ID_DAUSACH`, `ATTRIBUTE_22`) VALUES ('118', '40', 'sẵn có');
+INSERT INTO sach (`ID_SACH`, `ID_DAUSACH`, `ATTRIBUTE_22`) VALUES ('119', '40', 'sẵn có');
+INSERT INTO sach (`ID_SACH`, `ID_DAUSACH`, `ATTRIBUTE_22`) VALUES ('120', '40', 'sẵn có');
+INSERT INTO sach (`ID_SACH`, `ID_DAUSACH`, `ATTRIBUTE_22`) VALUES ('121', '41', 'sẵn có');
+INSERT INTO sach (`ID_SACH`, `ID_DAUSACH`, `ATTRIBUTE_22`) VALUES ('122', '41', 'sẵn có');
+INSERT INTO sach (`ID_SACH`, `ID_DAUSACH`, `ATTRIBUTE_22`) VALUES ('123', '41', 'sẵn có');
+INSERT INTO sach (`ID_SACH`, `ID_DAUSACH`, `ATTRIBUTE_22`) VALUES ('124', '42', 'sẵn có');
+INSERT INTO sach (`ID_SACH`, `ID_DAUSACH`, `ATTRIBUTE_22`) VALUES ('125', '42', 'sẵn có');
+INSERT INTO sach (`ID_SACH`, `ID_DAUSACH`, `ATTRIBUTE_22`) VALUES ('126', '42', 'sẵn có');
+INSERT INTO sach (`ID_SACH`, `ID_DAUSACH`, `ATTRIBUTE_22`) VALUES ('127', '43', 'sẵn có');
+INSERT INTO sach (`ID_SACH`, `ID_DAUSACH`, `ATTRIBUTE_22`) VALUES ('128', '43', 'sẵn có');
+INSERT INTO sach (`ID_SACH`, `ID_DAUSACH`, `ATTRIBUTE_22`) VALUES ('129', '43', 'sẵn có');
+INSERT INTO sach (`ID_SACH`, `ID_DAUSACH`, `ATTRIBUTE_22`) VALUES ('130', '44', 'sẵn có');
+INSERT INTO sach (`ID_SACH`, `ID_DAUSACH`, `ATTRIBUTE_22`) VALUES ('131', '44', 'sẵn có');
+INSERT INTO sach (`ID_SACH`, `ID_DAUSACH`, `ATTRIBUTE_22`) VALUES ('132', '44', 'sẵn có');
+INSERT INTO sach (`ID_SACH`, `ID_DAUSACH`, `ATTRIBUTE_22`) VALUES ('133', '45', 'sẵn có');
+INSERT INTO sach (`ID_SACH`, `ID_DAUSACH`, `ATTRIBUTE_22`) VALUES ('134', '45', 'sẵn có');
+INSERT INTO sach (`ID_SACH`, `ID_DAUSACH`, `ATTRIBUTE_22`) VALUES ('135', '45', 'sẵn có');
+
+
+
+INSERT INTO chitiet_dausach_tacgia (`ID_DAUSACH`, `ID_TACGIA`) VALUES ('31', '36');
+INSERT INTO chitiet_dausach_tacgia (`ID_DAUSACH`, `ID_TACGIA`) VALUES ('32', '37');
+INSERT INTO chitiet_dausach_tacgia (`ID_DAUSACH`, `ID_TACGIA`) VALUES ('33', '38');
+INSERT INTO chitiet_dausach_tacgia (`ID_DAUSACH`, `ID_TACGIA`) VALUES ('34', '39');
+INSERT INTO chitiet_dausach_tacgia (`ID_DAUSACH`, `ID_TACGIA`) VALUES ('35', '40');
+INSERT INTO chitiet_dausach_tacgia (`ID_DAUSACH`, `ID_TACGIA`) VALUES ('36', '41');
+INSERT INTO chitiet_dausach_tacgia (`ID_DAUSACH`, `ID_TACGIA`) VALUES ('37', '42');
+INSERT INTO chitiet_dausach_tacgia (`ID_DAUSACH`, `ID_TACGIA`) VALUES ('38', '43');
+INSERT INTO chitiet_dausach_tacgia (`ID_DAUSACH`, `ID_TACGIA`) VALUES ('39', '44');
+INSERT INTO chitiet_dausach_tacgia (`ID_DAUSACH`, `ID_TACGIA`) VALUES ('40', '45');
+INSERT INTO chitiet_dausach_tacgia (`ID_DAUSACH`, `ID_TACGIA`) VALUES ('41', '46');
+INSERT INTO chitiet_dausach_tacgia (`ID_DAUSACH`, `ID_TACGIA`) VALUES ('42', '47');
+INSERT INTO chitiet_dausach_tacgia (`ID_DAUSACH`, `ID_TACGIA`) VALUES ('43', '48');
+INSERT INTO chitiet_dausach_tacgia (`ID_DAUSACH`, `ID_TACGIA`) VALUES ('44', '49');
+INSERT INTO chitiet_dausach_tacgia (`ID_DAUSACH`, `ID_TACGIA`) VALUES ('45', '50');
