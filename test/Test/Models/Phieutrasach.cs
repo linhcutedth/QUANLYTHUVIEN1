19c2d8c75e712722994c3b2335f41485880ecdf4
linhcutedth <19521757@gm.uit.edu.vn>
2022-05-26 09:28:54 +0700

    model_updaate

--- test/Test/Models/Phieutrasach.cs
+++ test/Test/Models/Phieutrasach.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 // Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
 // If you have enabled NRTs for your project, then un-comment the following line:
@@ -14,11 +15,15 @@ namespace Test.Models
             ChitietPts = new HashSet<ChitietPts>();
             Phieuthutienphat = new HashSet<Phieuthutienphat>();
         }
-
+        [Display(Name = "Mã phiếu trả sách")]
         public int IdPts { get; set; }
+        [Display(Name = "Mã độc giả")]
         public int IdDg { get; set; }
+        [Display(Name = "Ngày trả")]
         public DateTime? Ngtra { get; set; }
+        [Display(Name = "Tiền phạt kì này")]
         public decimal? Tienphatkinay { get; set; }
+        [Display(Name = "Tổng nợ")]
         public decimal? Tongno { get; set; }
 
         public virtual Thedocgia IdDgNavigation { get; set; }
